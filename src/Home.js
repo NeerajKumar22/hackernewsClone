7563eb7cb842083dfe676a45adfe57099da94f82
Neeraj Kumar <ineerajkr@gmail.com>
2019-06-10 19:21:26 +0530

    added home component

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './styles/index.scss';
+import Item from "./Item"
+
+const Home = () => {
+    return(
+        <>
+            <Item />
+        </>
+    )
+}
+
+export default Home;
